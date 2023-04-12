0490efcce0fcbc822ed91b73b8d7964180fe0647
yamamamo <yamamamamo@gmail.com>
2023-04-13 01:27:37 +0900

    호이스팅

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,11 +28,27 @@ class MainActivity : ComponentActivity() {
 
 //컴포저블 재사용
 @Composable
-private fun MyApp(
+private fun MyApp(modifier: Modifier = Modifier)
+{
+    var shouldShowOnboarding by remember { mutableStateOf(true)}
+
+    Surface(modifier) {
+        if(shouldShowOnboarding){
+            OnBoardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+            //상태가 아닌 함수를 OnBoardingScreen 에 전달하는 방식.
+            // 이 컴포저블의 재사용 가능성을 높이고 다른 컴포저블이 상태를 변경하지 않도록 보호
+        }else{
+            Greetings()
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
-) {
-    Column(modifier) {
+){
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
@@ -80,10 +92,49 @@ fun Greeting(name: String) {
     }
 }
 
+@Composable
+fun  OnBoardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit
+){
+//    var shouldShowOnboarding by remember { mutableStateOf(true)}
+    //= 대신 by 키워드를 사용. by 는 매번 .value를 입력할 필요가 없도록 해주는 속성 위임
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment =  Alignment.CenterHorizontally
+    ) {
+        Text(text = "반갑다는 기초 코드랩")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("께속")
+       }
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeSampleTheme(){
+        OnBoardingScreen(onContinueClicked = {})
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeSampleTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposeSampleTheme() {
+        MyApp(Modifier.fillMaxSize())
     }
 }
