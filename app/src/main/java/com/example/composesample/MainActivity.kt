cfc92b98fb93216af011778f42ad8bb5f5415390
yamamamo <yamamamamo@gmail.com>
2023-04-14 17:39:27 +0900

    ElevateButton -> IconButton 으로 변경

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -7,15 +7,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,17 +44,16 @@ class MainActivity : ComponentActivity() {
 
 //컴포저블 재사용
 @Composable
-private fun MyApp(modifier: Modifier = Modifier)
-{
+private fun MyApp(modifier: Modifier = Modifier) {
 //    var shouldShowOnboarding by remember { mutableStateOf(true)}
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true)}
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
-        if(shouldShowOnboarding){
-            OnBoardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+        if (shouldShowOnboarding) {
+            OnBoardingScreen(onContinueClicked = { shouldShowOnboarding = false })
             //상태가 아닌 함수를 OnBoardingScreen 에 전달하는 방식.
             // 이 컴포저블의 재사용 가능성을 높이고 다른 컴포저블이 상태를 변경하지 않도록 보호
-        }else{
+        } else {
             Greetings()
         }
     }
@@ -57,10 +63,10 @@ private fun MyApp(modifier: Modifier = Modifier)
 private fun Greetings(
     modifier: Modifier = Modifier,
 //    names: List<String> = listOf("World", "Compose")
-    names: List<String> = List(1000){"$it"}
-){
+    names: List<String> = List(1000) { "$it" }
+) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items(items = names) {name ->
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
@@ -71,10 +77,10 @@ fun Greeting(name: String) {
     //리컴포지션 - 컴포즈앱은 구성 가능한 함수를 호출하여 데이터를 ui로 변환합니다.
     //데이터가 변경되면 컴포즈는 새 데이터로 이러한 함수를 다시 실행하여 업데이트된 ui를 만듭니다.
     //remember 는 리컴포지션을 방지하는 데 사용되므로 상태가 재설정되지 않습니다.
-    val expanded = remember{ mutableStateOf(false)}
+    var expanded by remember { mutableStateOf(false) }
 
     val extraPadding by animateDpAsState(
-        if(expanded.value) 48.dp else 0.dp,
+        if (expanded) 48.dp else 0.dp,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioHighBouncy,
             stiffness = Spring.StiffnessLow
@@ -91,7 +97,7 @@ fun Greeting(name: String) {
         //Modifier 수정자. 수정자는 상위 요소 레이아웃 내에서 UI 요소가
         //배치되고 표시되고 동작하는 방식을 UI요소에 알려줍니다.
 
-        Row(modifier = Modifier.padding(24.dp)){
+        Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
@@ -104,35 +110,57 @@ fun Greeting(name: String) {
                         fontWeight = FontWeight.ExtraBold
                     )
                 )
+
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
             }
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value  }
-            ) {
-                Text(if(expanded.value) "하이하이" else "아니하이")
+            Box(modifier = Modifier.clickable { expanded = !expanded }){
+                IconButton(
+                    onClick = { expanded = !expanded }, enabled = true
+                ) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        contentDescription = stringResource(R.string.hi_hi)
+                    )
+                }
             }
+
+
+//            ElevatedButton(
+//                onClick = { expanded = !expanded  }
+//            ) {
+//                Text(if(expanded) stringResource(R.string.hi_hi) else stringResource(R.string.no_hi))
+//            }
+
+
+
         }
     }
 }
 
 @Composable
-fun  OnBoardingScreen(
+fun OnBoardingScreen(
     modifier: Modifier = Modifier,
     onContinueClicked: () -> Unit
-){
+) {
 //    var shouldShowOnboarding by remember { mutableStateOf(true)}
     //= 대신 by 키워드를 사용. by 는 매번 .value를 입력할 필요가 없도록 해주는 속성 위임
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment =  Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "반갑다는 기초 코드랩")
+        Text(text = stringResource(R.string.glad_easy_codelab))
         Button(
             modifier = Modifier.padding(vertical = 24.dp),
             onClick = onContinueClicked
         ) {
-            Text("께속")
-       }
+            Text(stringResource(R.string.ccontinue))
+        }
     }
 }
 
@@ -140,14 +168,15 @@ fun  OnBoardingScreen(
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
-    ComposeSampleTheme(){
+    ComposeSampleTheme() {
         OnBoardingScreen(onContinueClicked = {})
     }
 }
+
 @Preview(
     showBackground = true,
     widthDp = 320,
-    uiMode =UI_MODE_NIGHT_YES,
+    uiMode = UI_MODE_NIGHT_YES,
     name = "Dark"
 )
 @Preview(showBackground = true, widthDp = 320)
